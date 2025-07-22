7d6c5a9c872de4478542238bc188b44977e3e401
OmBarad1 <ombarad808@gmail.com>
2025-07-22 15:54:54 +0530

    My Cart

--- src/store/store.js
+++ src/store/store.js
@@ -3,14 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import ProductSlice from "./slice/ProductSlice";
 import UserSlice from "./slice/UserSlice";
 import CartSlice from "./slice/CartSlice";
-import AddressSlice from "./slice/addressSlice";
+// import AddressSlice from "./slice/addressSlice";
 
 export const store = configureStore({
   reducer: {
     products: ProductSlice,
     user: UserSlice,
     cart: CartSlice,
-    address: AddressSlice,
+    address: CartSlice,
   },
 });
 
