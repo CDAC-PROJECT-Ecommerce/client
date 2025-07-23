d7c4766420d1d6eb68487495766b7862cc25f399
AadityaTambuskar <aaditya.au.tambuskar@gmail.com>
2025-07-24 00:09:17 +0530

    Delete client directory

--- client/src/admin/constants/orderStatus.js
+++
@@ -1,1 +0,0 @@
-export const ORDER_STATUS_STEPS = ["Pending", "Shipped", "On The Way", "Delivered"];
--- client/src/admin/utils/orderStatusUtils.js
+++
@@ -1,7 +0,0 @@
-import { ORDER_STATUS_STEPS } from "../constants/orderStatus";
-
-export const getStatusOrderMap = () =>
-  ORDER_STATUS_STEPS.reduce((acc, step, index) => {
-    acc[step] = index + 1;
-    return acc;
-  }, {});
