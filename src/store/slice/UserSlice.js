488505b5ae65eb513192c2f080d3e64f650095ac
Ravi kumar <elravisingh@gmail.com>
2025-08-01 00:08:13 +0530

    Integrated login and signup with backend

--- src/store/slice/UserSlice.js
+++ src/store/slice/UserSlice.js
@@ -1,29 +1,66 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 import toast from "react-hot-toast";
+import { api } from "../../services/api";
 
-export const registerUser = createAsyncThunk("users/register", async (data) => {
-  return data;
-});
-
-export const loginUser = createAsyncThunk("users/login", async (data) => {
-  const { username, password } = data;
-  console.log(data);
-  if (username === "demo" && password === "1234") {
-    toast.success("Login sucessfull");
-    return { username: "Ravi", token: "laksdjfo0sudfafsoah0a7sfd0" };
-  } else {
-    toast.error("login failed");
+export const registerUser = createAsyncThunk(
+  "users/register",
+  async (data, { rejectWithValue }) => {
+    try {
+      const response = await api.post("/api/auth/register", data);
+      return response.data;
+    } catch (error) {
+      const status = error.response?.status;
+      const message = error.response?.data?.message;
+      return rejectWithValue({ status, message });
+    }
   }
-});
+);
+
+//Fetch username and role if user is already logged in
+export const fetchUsernameAndRole = createAsyncThunk(
+  "users/username",
+  async (data, { rejectWithValue }) => {
+    try {
+      data = { token: JSON.parse(data) };
+      const response = await api.post("/api/auth/fetchUsernameAndRole", data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      return response.data;
+    } catch (error) {
+      const status = error.response?.status;
+      const message = error.response?.data?.message;
+      return rejectWithValue({ status, message });
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "users/login",
+  async (data, { rejectWithValue }) => {
+    try {
+      console.log(data);
+      const response = await api.post("/api/auth/login", data);
+      return response.data;
+    } catch (error) {
+      const status = error.response?.status;
+      const message = error.response?.data?.message;
+      return rejectWithValue({ status, message });
+    }
+  }
+);
 
 const UserSlice = createSlice({
   name: "users",
   initialState: {
-    Users: [],
+    userToken: null,
+    role: "USER",
+    username: "",
     TempUsers: [],
     isLoading: false,
     isLoggedIn: false,
-    isError: false,
     toastMessage: "",
   },
   reducers: {
@@ -36,24 +73,87 @@ const UserSlice = createSlice({
     changePassword(state, action) {
       state.password = action.payload;
     },
+
+    // Fetch token from localstorage
+    fetchToken(state, action) {
+      const token = localStorage.getItem("shopee_token") || null;
+      if (token !== null) {
+        state.userToken = token;
+      }
+    },
+    logout(state, action) {
+      localStorage.removeItem("shopee_token");
+      state.userToken = null;
+      state.username = "";
+      state.role = "USER";
+      toast.success("Logout successfull");
+    },
   },
   extraReducers: (builder) => {
-    // builder.addCase(registerUser.fulfilled, (state, action) => {
-    //   state.isLoading = false;
-    //   state.Users = action.payload;
-    // });
+    builder.addCase(loginUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(loginUser.rejected, (state, action) => {
+      state.isLoading = false;
+      const { status, message } = action.payload || {};
+      if (status === 401) {
+        toast.error("Invalid username/password");
+      } else {
+        toast.error(message || "Login failed");
+      }
+    });
+
     builder.addCase(loginUser.fulfilled, (state, action) => {
       state.isLoading = false;
-      state.Users = action.payload;
+      const responseData = action.payload;
+
+      localStorage.setItem("shopee_token", JSON.stringify(responseData.token));
+      state.userToken = JSON.stringify(responseData.token);
+      state.username = responseData.username;
+      toast.success("Login successfully");
+    });
+
+    builder.addCase(registerUser.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(registerUser.rejected, (state, action) => {
+      state.isLoading = false;
+      const { status, message } = action.payload || {};
+      if (status === 409) {
+        toast.error("User already exists");
+      } else {
+        toast.error(message || "Registration failed");
+      }
+
+      state.isError = true;
     });
 
     builder.addCase(registerUser.fulfilled, (state, action) => {
       state.isLoading = false;
-      toast.success("User registered");
-      state.TempUsers = [...state.Users, action.payload];
+      const responseData = action.payload;
+
+      localStorage.setItem("shopee_token", JSON.stringify(responseData.token));
+      state.userToken = JSON.stringify(responseData.token);
+      state.username = responseData.username;
+      toast.success("Registered successfully");
+    });
+
+    // builder.addCase(fetchUsernameAndRole.pending, (state, action) => {
+    //   state.isLoading = true;
+    // });
+    // builder.addCase(fetchUsernameAndRole.rejected, (state, action) => {
+    //   state.isLoading = false;
+    //  toast.error("Something went wrong")
+    // });
+
+    builder.addCase(fetchUsernameAndRole.fulfilled, (state, action) => {
+      console.log("first");
+      state.username = action.payload.username;
+      state.role = action.payload.role;
     });
   },
 });
 
-export const { updateProfile, changePassword } = UserSlice.actions;
+export const { fetchToken, logout, updateProfile, changePassword } =
+  UserSlice.actions;
 export default UserSlice.reducer;
