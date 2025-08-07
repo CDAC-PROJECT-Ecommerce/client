f57b375b1eb8d70012549224832b81058b719ace
Yash Rajeshwar Shirbhate <yashshirbhatesl@gmail.com>
2025-08-07 20:59:13 +0530

    Review Client Part

---
+++ src/store/slice/ReviewSlice.js
@@ -0,0 +1,59 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { api } from '../../services/api';
+import toast from 'react-hot-toast';
+
+export const createReview = createAsyncThunk(
+    'review/createReview',
+    async (reviewData, { getState, rejectWithValue }) => {
+        try {
+            const state = getState();
+            const token = JSON.parse(state?.user.userToken);
+            const response = await api.post(`/api/reviews`, reviewData, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+            });
+            return response.data;
+        } catch (error) {
+            toast.error(error?.response?.data)
+            return rejectWithValue(error.message);
+        }
+    }
+);
+
+const initialState = {
+    reviews: [],
+    currentReview: null,
+    hasReviewed: false,
+    status: {
+        create: { loading: false, error: null, success: false },
+    }
+};
+
+const ReviewSlice = createSlice({
+    name: 'review',
+    initialState,
+    reducers: {
+        resetReviewStatus: (state) => {
+            state.status.create = initialState.status.create;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(createReview.pending, (state) => {
+                state.status.create = { loading: true, error: null, success: false };
+            })
+            .addCase(createReview.fulfilled, (state, action) => {
+                state.status.create = { loading: false, error: null, success: true };
+                state.reviews.push(action.payload);
+                toast.success("Review submitted");
+                state.hasReviewed = true;
+            })
+            .addCase(createReview.rejected, (state, action) => {
+                state.status.create = { loading: false, error: action.payload, success: false };
+            });
+    }
+});
+
+export const { resetReviewStatus } = ReviewSlice.actions;
+export default ReviewSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,12 @@
-// app/store.js
 import { configureStore } from "@reduxjs/toolkit";
 import ProductSlice from "./slice/ProductSlice";
 import UserSlice from "./slice/UserSlice";
 import CartSlice from "./slice/CartSlice";
 import AddressSlice from "./slice/addressSlice";
-import OrdersSlice from "./slice/OrdersSlice"
+import OrdersSlice from "./slice/OrdersSlice";
 import UserOrderSlice from "./slice/UserOrderSlice";
 import UserProfile from "./slice/UserProfile";
+import reviewReducer from './slice/ReviewSlice';
 
 export const store = configureStore({
   reducer: {
@@ -16,7 +16,8 @@ export const store = configureStore({
     orders: OrdersSlice,
     address: AddressSlice,
     userOrder: UserOrderSlice,
-    userProfile:UserProfile
+    userProfile: UserProfile,
+    review: reviewReducer
   },
 });
 
